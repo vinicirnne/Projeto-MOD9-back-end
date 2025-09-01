aef494b9e759c8219ee5021db93f31adb12fda78
Dominique <domii.ju00@gmail.com>
2025-09-01 14:55:07 -0300

    Otimização no codigo do nome dos cardeais

--- AtividadeConclave.js
+++ AtividadeConclave.js
@@ -1,17 +1,12 @@
 const entrada = require('prompt-sync')({ sigint: false });
-//Interaçao com usuario para adicionar nomes ao cardeais
-let card1 = entrada("Nome primeiro cardeal; ");
-let card2 = entrada("Nome segundo cardeal; ");
-let card3 = entrada("Nome terceiro cardeal; ");
-let card4 = entrada("Nome quarto cardeal; ");
-let card5 = entrada("Nome quinto cardeal; ");
-//Menu que mostra nome + numero dos cardeais da votação
-console.log(card1 + " - " + 1);
-console.log(card2 + " - " + 2);
-console.log(card3 + " - " + 3);
-console.log(card4 + " - " + 4);
-console.log(card5 + " - " + 5);
-do {
+let cardeais = []
+for(let i=1; i <=5; i++){
+    cardeal = entrada("Digite nome do cardeal " + i + ":")
+    cardeais.push(cardeal);
+}
+cardeais.forEach((cardeal, i) => console.log(`${i + 1} - ${cardeal}`)); 
+
+ do {
     //Variaveis para que ocorra a votação
     let c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0;
     //Estrutura de repetição para votação secreta
@@ -41,15 +36,15 @@ do {
     if (encerrar.trim() !== "") {
         //Contagem de votos para decisão do vencendor ou se irá reiniciar votação
         if (c1 >= 4) {
-            console.log("O PAPA ELEITO É; " + card1); break;
+            console.log("O PAPA ELEITO É; " + cardeais[0]); break;
         } else if (c2 >= 4) {
-            console.log("O PAPA ELEITO É; " + card2); break;
+            console.log("O PAPA ELEITO É; " + cardeais[1]); break;
         } else if (c3 >= 4) {
-            console.log("O PAPA ELEITO É; " + card3); break;
+            console.log("O PAPA ELEITO É; " + cardeais[2]); break;
         } else if (c4 >= 4) {
-            console.log("O PAPA ELEITO É; " + card4); break;
+            console.log("O PAPA ELEITO É; " + cardeais[3]); break;
         } else if (c5 >= 4) {
-            console.log("O PAPA ELEITO É; " + card5); break;
+            console.log("O PAPA ELEITO É; " + cardeais[4]); break;
         } else {
             console.log("NENHUM FOI ELEITO!!! reiniciando votação até sair um vencendor")
         }
